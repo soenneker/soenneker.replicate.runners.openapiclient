5bcf9f551e749595939db6ae4580b1817c3590ad
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:34 -0500

    TUnit conversion

--- test/Soenneker.Replicate.Runners.OpenApiClient.Tests/ReplicateOpenApiClientRunnerTests.cs
+++ test/Soenneker.Replicate.Runners.OpenApiClient.Tests/ReplicateOpenApiClientRunnerTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Replicate.Runners.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class ReplicateOpenApiClientRunnerTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class ReplicateOpenApiClientRunnerTests : HostedUnitTest
 {
-    public ReplicateOpenApiClientRunnerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public ReplicateOpenApiClientRunnerTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
